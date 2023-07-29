f49044be99896584d37b1c7d9573facf5ff70796
MariyaIlina <mariy4ilina@yandex.ru>
2023-07-29 20:49:58 +0400

    port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { errors } = require('celebrate');
 const { routes } = require('./routes/index');
 const { limiter } = require('./middlewares/limiter');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
